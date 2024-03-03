f88508463c088d57a1f2771e1ef1061a3a55f143
duh-git <cherebeevkarim2015@gmail.com>
2024-03-03 20:12:12 +0300

    remote data

--- data.js
+++ data.js
@@ -1,26 +1,69 @@
-export const data = {
-  sliderData: [
-    {
-      src: './slider-img/img-1.jpg',
-      title: 'WINTER SALE ДО -70%',
-    },
-    {
-      src: './slider-img/img-2.jpg',
-      title: 'WINTER SALE ДО -70%',
-    },
-    {
-      src: './slider-img/img-3.jpg',
-      title: 'LIB TECH',
-    },
-  ],
-  promoData: [
-    {
-      src: './promo/promo-1.png',
-      title: 'НАЗВАНИЕ АКЦИИ №1',
-    },
-    {
-      src: './promo/promo-2.png',
-      title: 'НАЗВАНИЕ АКЦИИ №2',
-    },
-  ],
-}
+export const sliderData = [
+  {
+    src: './slider-img/img-1.jpg',
+    title: 'WINTER SALE ДО -70%',
+  },
+  {
+    src: './slider-img/img-2.jpg',
+    title: 'WINTER SALE ДО -70%',
+  },
+  {
+    src: './slider-img/img-3.jpg',
+    title: 'LIB TECH',
+  },
+];
+
+export const brendData = [
+  { src: './brends/brend-1.svg' },
+  { src: './brends/brend-2.svg' },
+  { src: './brends/brend-3.svg' },
+  { src: './brends/brend-4.svg' },
+  { src: './brends/brend-5.svg' },
+  { src: './brends/brend-6.svg' },
+  { src: './brends/brend-7.svg' },
+  { src: './brends/brend-8.svg' },
+  { src: './brends/brend-9.svg' },
+  { src: './brends/brend-10.svg' },
+];
+
+export const promoData = [
+  {
+    src: './promo/promo-1.png',
+    title: 'НАЗВАНИЕ АКЦИИ №1',
+  },
+  {
+    src: './promo/promo-2.png',
+    title: 'НАЗВАНИЕ АКЦИИ №2',
+  },
+];
+
+export const catalogData = [
+  {
+    src: './catalog/board-1.png',
+    favorite: true,
+    title: 'LIB TECH',
+    category: 'Мужской Сноуборд',
+    cost: '34392 ₽',
+  },
+  {
+    src: './catalog/board-2.png',
+    favorite: false,
+    title: 'LIB TECH',
+    category: 'Мужской Сноуборд',
+    cost: '34392 ₽',
+  },
+  {
+    src: './catalog/board-3.png',
+    favorite: false,
+    title: 'LIB TECH',
+    category: 'Мужской Сноуборд',
+    cost: '34392 ₽',
+  },
+  {
+    src: './catalog/board-4.png',
+    favorite: false,
+    title: 'LIB TECH',
+    category: 'Мужской Сноуборд',
+    cost: '34392 ₽',
+  },
+];
